f99ec3abbac8c15d29dc642443ac8f204bb43268
Stephen Carr <Stephen.Carr@GM.com>
2018-11-21 21:25:00 -0600

    Added bootstrap 

--- ttt-ui/src/app/app.component.ts
+++ ttt-ui/src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { Observable } from "rxjs"
+
+class Title {
+  name;
+
+}
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
+  constructor(private http : HttpClient){}
   title = 'ttt-ui';
+
+  ngOnInit(){
+    this.getTitle().subscribe(
+      x => this.title = x.name,
+      x => console.log(x),
+      () => console.log("Finished call")
+    );
+  }
+
+  getTitle(): Observable<Title> {
+    const url = "http:\\www.localhost:8080";
+    return this.http.get<Title>(url);
+  }
 }
